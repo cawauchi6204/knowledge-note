64991e2eb8ef3aac9b2a4ce3c19709578080965d
cawauchi6204 <cawauchi6204@gmail.com>
2021-02-23 14:31:44 +0900

    feat(components/Layout.tsx): レイアウトコンポーネントの作成

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import styled from 'styled-components'
 
+import Head from './Head'
 import Header from "./Header"
 import Footer from "./Footer"
 
@@ -13,11 +14,22 @@ const Section = styled.section`
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
-      <Header />
-      <Section>
-        {children}
-      </Section>
-      <Footer />
+      <Head
+        title={'タイトルです'}
+        description={'ディスクリプションです'}
+        keyword={'キーワードです！'}
+        image={
+          'https://qiita-user-profile-images.imgix.net/https%3A%2F%2Flh3.googleusercontent.com%2F-BUmWankl_aQ%2FAAAAAAAAAAI%2FAAAAAAAADys%2F8oi87glPMLA%2Fphoto.jpg%3Fsz%3D50?ixlib=rb-1.2.2&auto=compress%2Cformat&lossless=0&w=300&s=649d309c71e365e6fc6d6b6d205c3710'
+        }
+        url={'https://qiita.com/shinshin86'}
+      />
+      <body>
+        <Header />
+        <Section>
+          {children}
+        </Section>
+        <Footer />
+      </body>
     </>
   )
 }
