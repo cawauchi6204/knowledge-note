3d6903debee62d1148cf3317ea64e8e91eaabddb
cawauchi6204 <cawauchi6204@gmail.com>
2021-02-27 22:20:41 +0900

    feat(components/card cardList.tsx): カードコンポーネントのサイズの修正

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useContext } from "react"
 import styled from 'styled-components'
+import CommonReducerContext from '../common/CommonContext'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -10,17 +11,20 @@ type Props = {
     updated: string
     id: string
     eyecatching: {
-      url:string
+      url: string
     }
-  }
+  },
 }
 
-const Article = styled.article`
+const Article: any = styled.article.attrs((props: any) => ({
+  width: props.states.isSmartPhone || props.states.isTablet ? 'auto' : '300px',
+  height: props.states.isSmartPhone || props.states.isTablet ? 'auto' : 'auto',
+}))`
   border-radius:10px;
-  width:300px;
-  height:300px;
+  width:${props => props.width};
+  height:${props => props.height};
   box-shadow: 0 1px 8px 0 rgba(0,0,0,.15);
-  `
+`
 
 const Content = styled.div`
   padding:4px;
@@ -42,9 +46,10 @@ display:inline-block;
 `
 
 const Card: React.FC<Props> = ({ description }) => {
+  const states: any = useContext(CommonReducerContext)
   return (
     <Link href="/blogs/[id]" as={`blogs/${description.id}`}>
-      <Article className="pointer">
+      <Article className="pointer" states={states}>
         {
           description.eyecatching ?
             <Image width="auto" height="200px" src={description.eyecatching.url} />
--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useContext } from "react"
 import styled from 'styled-components'
+import CommonReducerContext from '../common/CommonContext'
 
 import Card from './Card'
 
@@ -23,25 +24,87 @@ type Props = {
   ],
 }
 
-const Grid = styled.article`
-margin: 30px 0 60px;
-    display: grid;
-    grid-template-columns: repeat(3,1fr);
-    grid-row-gap: 15px;
-    grid-column-gap: 15px;
-    -webkit-column-gap: 15px;
-    column-gap: 15px;
+const CardListSection: any = styled.section.attrs((props: any) => ({
+  margin: props.states.isSmartPhone || props.states.isTablet ? '7% 0 60px;' : '0 auto',
+}))`
+  margin:${props => props.margin};
+`
+
+const PCGrid = styled.article`
+  margin: 7% auto;
+  max-width:1032px;
+  display: grid;
+  place-items:center;
+  grid-template-columns: repeat(3,1fr);
+  justify-content:center;
+  grid-row-gap: 15px;
+  grid-column-gap: 15px;
+  -webkit-column-gap: 15px;
+  column-gap: 15px;
+  row-gap:15px;
+`
+
+const TBGrid = styled.article`
+  margin-top:7%;
+  margin: 30px 0 60px;
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  grid-row-gap: 15px;
+  grid-column-gap: 15px;
+  -webkit-column-gap: 15px;
+  column-gap: 15px;
+`
+
+const SPGrid = styled.article`
+  margin-top:7%;
+  margin: 30px 0 60px;
+  display: grid;
+  grid-template-columns: repeat(1,1fr);
+  grid-row-gap: 15px;
+  grid-column-gap: 15px;
+  -webkit-column-gap: 15px;
+  column-gap: 15px;
 `
 
 const CardList: React.FC<Props> = ({ blogs }) => {
+  const states: any = useContext(CommonReducerContext)
+
+  const {
+    isSmartPhone,
+    isTablet,
+    isPc
+  } = states
+  // const {state, dispatch, isSmartPhone, isTablet, isPc} = states
   return (
-    <Grid>
-      {blogs.map((blog: any) => (
-        <React.Fragment key={blog.id}>
-          <Card description={blog} />
-        </React.Fragment>
-      ))}
-    </Grid>
+    <CardListSection states={states}>
+      {isSmartPhone && (
+        <SPGrid>
+          {blogs.map((blog: any) => (
+            <React.Fragment key={blog.id}>
+              <Card description={blog} />
+            </React.Fragment>
+          ))}
+        </SPGrid>
+      )}
+      {isTablet && (
+        <TBGrid>
+          {blogs.map((blog: any) => (
+            <React.Fragment key={blog.id}>
+              <Card description={blog} />
+            </React.Fragment>
+          ))}
+        </TBGrid>
+      )}
+      {isPc && (
+        <PCGrid>
+          {blogs.map((blog: any) => (
+            <React.Fragment key={blog.id}>
+              <Card description={blog} />
+            </React.Fragment>
+          ))}
+        </PCGrid>
+      )}
+    </CardListSection>
   )
 }
 
