305f1b5c83d701baa72939596e6ac6f78d194d5f
cawauchi6204 <cawauchi6204@gmail.com>
2021-02-27 22:24:37 +0900

    feat(components/CoDevelopment.tsx): サンプル共同開発セクションコンポーネントの作成

---
+++ src/components/CoDevelopment.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Image from 'next/image'
+
+type Props = {}
+
+const CoDevelopment: React.FC<Props> = ({ }) => {
+  return (
+    <>
+      <Image width="600" height="auto" src="https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1353&q=80" />
+    </>
+  )
+}
+
+export default CoDevelopment
