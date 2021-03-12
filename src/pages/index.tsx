c82b088417d4567bae2d106fcc90b75bc9466c75
cawauchi6204 <cawauchi6204@gmail.com>
2021-03-12 21:42:54 +0900

    feat(page/[offset].tsx pages/index.tsx): ページネーション実装
    #4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import React, { useContext } from 'react'
+import React, { useContext, useCallback } from 'react'
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import { Pagination } from '@material-ui/lab'
+import { Grid } from '@material-ui/core'
 
 import CommonProvider from '../common/CommonProvider'
 import CommonReducerContext from '../common/CommonContext'
 import Layout from '../components/Layout'
 import CardList from '../components/CardList'
-import CoDevelopment from '../components/CoDevelopment'
 
 type Props = {
   blogs: [
@@ -21,45 +24,64 @@ type Props = {
         url: string
         height: number
         width: number
-      }
+      },
     }
   ],
+  totalCount: number,
+  limit: number,
 }
 
-type Tag = {
-  id: string
-  createdAt: string
-  updatedAt: string
-  publishedAt: string
-  revisedAt: string
-  name: string
-}
 
-const Home: React.FC<Props> = ({ blogs }) => {
+
+const Home: React.FC<Props> = ({ blogs, totalCount, limit }) => {
   const { state, dispatch }: any = useContext(CommonReducerContext)
+  const router = useRouter()
+
+  const handleChangePage = useCallback(
+    (_: React.ChangeEvent<unknown>, page: number) => {
+      void router.push(`/page/${page}`)
+    },
+    [router],
+  )
+
   return (
     <CommonProvider>
       <Layout>
         <CardList blogs={blogs} />
-        <CoDevelopment />
+        <Grid container justify="center">
+          <Pagination
+            count={Math.ceil(totalCount / limit)}
+            variant="outlined"
+            shape="rounded"
+            color="secondary"
+            page={0}
+            onChange={handleChangePage}
+            className="center"
+          />
+        </Grid>
       </Layout>
     </CommonProvider>
   )
 }
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const key: any = {
     headers: { 'X-API-KEY': process.env.API_KEY },
   }
+  const LIMIT = 6
   const res = await fetch(
-    `https://knowledge-note.microcms.io/api/v1/blogs`,
+    `https://knowledge-note.microcms.io/api/v1/blogs?offset=0&limit=${LIMIT}`,
     key,
   )
   const data = await res.json()
+  const totalCount: number = data.totalCount
+  const limit: number = data.limit
 
   return {
     props: {
       blogs: data.contents,
+      totalCount,
+      limit
     }
   }
 }
