b2c4489269a12c7d63c9f1aceb2ba0f4f7177989
cawauchi6204 <cawauchi6204@gmail.com>
2021-03-13 14:02:29 +0900

    feat(components/Article.tsx): サイドバーのレスポンシブ対応
    Providerを使用してグローバル変数を参照
    
    #2,3,4

---
+++ src/components/Article.tsx
@@ -0,0 +1,95 @@
+import React, { useContext } from 'react'
+import CommonReducerContext from '../common/CommonContext'
+import styled from 'styled-components'
+import cheerio from 'cheerio'
+import 'highlight.js/styles/night-owl.css'
+
+import Toc from '../components/Toc'
+import Sidebar from '../components/Sidebar'
+import { CommonFlexRow } from '../common/CommonHtmlStyledTag'
+
+type Props = {
+  blog: {
+    id: string,
+    createdAt: string,
+    updatedAt: string,
+    publishedAt: string,
+    revisedAt: string,
+    title: string,
+    tags: any[],
+    body: any
+  },
+  highlightedBody: string
+}
+
+type Tag = {
+  id: string,
+  createdAt: string,
+  updatedAt: string,
+  publishedAt: string,
+  revisedAt: string,
+  name: string
+}
+
+const LeftColumn: any = styled.section.attrs((props: any) => ({
+  width: props.states.isSmartPhone || props.states.isTablet ? '100%' : '70%',
+}))`
+  width:${props => props.width};
+  height:100%;
+`
+
+const RightColumn = styled.section`
+  width:30%;
+  height:100%;
+`
+
+const Article: React.FC<Props> = ({ blog, highlightedBody }) => {
+  const states: any = useContext(CommonReducerContext)
+
+  const {
+    isSmartPhone,
+    isTablet,
+    isPc
+  } = states
+  console.log('Articleの55行目のstatesは' + JSON.stringify(states))
+  console.log('hogoehoge')
+  const $ = cheerio.load(highlightedBody)
+  const headings = $('h1, h2, h3').toArray()
+  const tocArray: any = headings.map((data: any) => ({
+    text: data.children[0].data,
+    id: data.attribs.id,
+    name: data.name
+  }))
+
+  return (
+    <CommonFlexRow>
+      <LeftColumn states={states}>
+        <h1>{blog.title}</h1>
+        <div>
+          {blog.tags.map((tag: Tag) => (
+            <React.Fragment key={tag.id}>
+              <span>{tag.name}</span>
+            </React.Fragment>
+          ))}
+        </div>
+        {
+          tocArray.length > 0 && <Toc tocArray={tocArray} />
+        }
+        <div dangerouslySetInnerHTML={{ __html: highlightedBody }}></div>
+      </LeftColumn>
+      {
+        isPc && (
+          <RightColumn>
+            <Sidebar>
+              {
+                tocArray.length > 0 && <Toc tocArray={tocArray} />
+              }
+            </Sidebar>
+          </RightColumn>
+        )
+      }
+    </CommonFlexRow>
+  )
+}
+
+export default Article
--- src/pages/blogs/[id].tsx
+++ src/pages/blogs/[id].tsx
@@ -1,15 +1,11 @@
-import React, { useContext } from 'react';
-import CommonReducerContext from '../../common/CommonContext'
-import styled from 'styled-components'
+import React from 'react';
 import cheerio from 'cheerio';
 import hljs from 'highlight.js'
 import 'highlight.js/styles/night-owl.css';
 
 import CommonProvider from '../../common/CommonProvider'
 import Layout from '../../components/Layout'
-import Toc from '../../components/Toc'
-import Sidebar from '../../components/Sidebar'
-import { CommonFlexRow } from '../../common/CommonHtmlStyledTag'
+import Article from '../../components/Article'
 
 type Props = {
   blog: {
@@ -34,51 +30,13 @@ type Tag = {
   name: string
 }
 
-const LeftColumn = styled.section`
-  width:70%;
-  height:100%;
-`
-
-const RightColumn = styled.section`
-  width:30%;
-  height:100%;
-`
-
 const BlogId: React.FC<Props> = ({ blog, highlightedBody }) => {
-  const { state, dispatch }: any = useContext(CommonReducerContext)
   const $ = cheerio.load(highlightedBody)
-  const headings = $('h1, h2, h3').toArray()
-  const tocArray: any = headings.map((data: any) => ({
-    text: data.children[0].data,
-    id: data.attribs.id,
-    name: data.name
-  }))
+
   return (
     <CommonProvider>
       <Layout>
-        <CommonFlexRow>
-          <LeftColumn>
-            <h1>{blog.title}</h1>
-            <div>
-              {blog.tags.map((tag: Tag) => (
-                <React.Fragment key={tag.id}>
-                  <span>{tag.name}</span>
-                </React.Fragment>
-              ))}
-            </div>
-            {
-              tocArray.length > 0 && <Toc tocArray={tocArray} />
-            }
-            <div dangerouslySetInnerHTML={{ __html: highlightedBody }}></div>
-          </LeftColumn>
-          <RightColumn>
-            <Sidebar>
-              {
-                tocArray.length > 0 && <Toc tocArray={tocArray} />
-              }
-            </Sidebar>
-          </RightColumn>
-        </CommonFlexRow>
+        <Article blog={blog} highlightedBody={highlightedBody}/>
       </Layout>
     </CommonProvider>
   )
